53d8bab887ecaf6dc627e2eb7d66f8b002ff1195
Vinicius Luiz Rodrigues <vinicius-luiz-rodrigues@outlook.com>
2026-08-21 22:23:49 -0300

    Audit BT11-106 Etemon Fusion
    - register the cataloged cost reduction, aura grant, and security reveal\n- allow the black-Tamer conditional cost reduction

--- apps/api/src/engine/effects/interpreter/registration/reducers.ts
+++ apps/api/src/engine/effects/interpreter/registration/reducers.ts
@@ -90,6 +90,7 @@ const VERIFIED_SELF_REDUCER_CARDS = new Set([
   "BT11-103", // condition: you have a green Tamer -> Option use cost -1
   "BT11-104", // condition: you have a green Tamer -> Option use cost -1
   "BT11-105", // condition: you have a Snatchmon -> Option use cost -1
+  "BT11-106", // condition: you have a black Tamer -> Option use cost -1
 ]);
 
 /**
