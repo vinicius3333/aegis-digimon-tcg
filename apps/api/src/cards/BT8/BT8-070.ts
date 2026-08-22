98a45626ea8259a49e4ad0cf72ffe9e1f4334748
Vinicius Luiz Rodrigues <vinicius-luiz-rodrigues@outlook.com>
2026-08-22 03:24:39 -0300

    Align BT8-070 effect classification

--- apps/api/src/cards/BT8/BT8-070.ts
+++ apps/api/src/cards/BT8/BT8-070.ts
@@ -20,7 +20,7 @@ const compiled: CompiledCard = {
     },
     {
       trigger: "AllTurns",
-      isInherited: true,
+      isInherited: false,
       frequency: "OncePerTurn",
       actions: [{
         kind: "SubTrigger",
