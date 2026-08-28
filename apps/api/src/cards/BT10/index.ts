47812f51c86f8f3b3a2e6c8393e16a1a4b9d0e3b
Vinicius Luiz Rodrigues <vinicius-luiz-rodrigues@outlook.com>
2026-08-28 02:53:59 -0300

    Register Gogmamon card module
    - load BT10-064 from the BT10 card barrel

--- apps/api/src/cards/BT10/index.ts
+++ apps/api/src/cards/BT10/index.ts
@@ -57,6 +57,7 @@ import "./BT10-060.js";
 import "./BT10-061.js";
 import "./BT10-062.js";
 import "./BT10-063.js";
+import "./BT10-064.js";
 import "./BT10-066.js";
 import "./BT10-067.js";
 import "./BT10-068.js";
