63ed6a1d820353c5f0a7a1b4aa7116dc3042f217
Vinicius Luiz Rodrigues <vinicius-luiz-rodrigues@outlook.com>
2026-08-29 11:20:43 -0300

    fix(bt23): clarify Erika hand return

--- apps/api/src/cards/BT23/BT23-084.ts
+++ apps/api/src/cards/BT23/BT23-084.ts
@@ -99,6 +99,7 @@ export const compiled: CompiledCard = {
                   },
                   count: 1,
                 },
+                to: "hand",
               },
             ],
             raw: "By suspending this Tamer and returning 1 of your Digimon with the [Hudie] trait to the hand",
